d7e4492733e60cca432353aa6408950f0b5c4826
Sathin Smakkamai <sathinsmakkamai@gmail.com>
2020-12-15 16:58:10 +0700

    add worksheet 5 QTpart

--- Worksheet5/ex2/mainwindow.h
+++ Worksheet5/ex2/mainwindow.h
@@ -1,23 +1,37 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "stockitemlistmodel.h"
 #include <QMainWindow>
 
-
 namespace Ui {
-class MainWindow;
+	class MainWindow;
 }
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+// Example of slot definition in mainwindow.h---------------------------
+	public slots:
+		void handleAddButton();
+		void handleEditButton();
+		void handleRemoveButton();
+		void on_actionSave_triggered();
+// ---------------------------------------------------------------------
 
-private:
-    Ui::MainWindow *ui;
+	signals:
+		void statusUpdateMessage( const QString & message, int timeout );
+
+
+	public:
+    		explicit MainWindow(QWidget *parent = 0);
+    		~MainWindow();
+
+
+	private:
+		StockItemListModel stockList;
+    		Ui::MainWindow *ui;
+
 };
-
 #endif // MAINWINDOW_H
