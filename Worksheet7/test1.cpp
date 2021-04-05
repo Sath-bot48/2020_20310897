e173f47d9a8d44fdffebaaf6dec1db5d61426399
Sathin Smakkamai <sathinsmakkamai@gmail.com>
2021-04-05 20:05:10 +0700

    new worksheet7

--- Worksheet7/test1.cpp
+++ Worksheet7/test1.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-	int a = 2;
-	int b = 5;
-	int c = add(a,b);
-	return (c == 7) ? 0 : 1;
+int a = 2;
+int b = 5;
+int c = add(a,b);
+return (c == 7) ? 0 : 1;
 }
